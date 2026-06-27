2510773205007c883cbe7cd5c0a0d285e5d4f389
Felipe Fagundes <ffelipemachado.10@gmail.com>
2026-06-27 18:22:48 -0300

    Inicio de criação do Menu

--- SISTEMAdeESTOQUE.py
+++ SISTEMAdeESTOQUE.py
@@ -2,6 +2,20 @@
 USUARIO = 'admin'
 SENHA = '123'
 ROTINA = 1
+## -------------------- FUNÇÃO MENU -------------------------------------------##
+def menu():
+    op = int(input('SELECIONE UMA OPÇÃO\n 1.Incluir item\n 2.Pesquisar item\n 3.Alterar item\n 4.Deletar item\n'))
+    if op == 1:
+        print('criar a funcao incluir')
+    elif op == 2:
+        print('criar a funcao pesquisar')
+    elif op == 3:
+        print('criar a funcao alterar')
+    elif op == 4:
+        print('criar a funcao deletar')
+    else:
+        print('opção inválida')
+        menu() #trocar por um while pra não ocupar memória
 
 while ROTINA == 1:
 
@@ -13,11 +27,15 @@ while ROTINA == 1:
     ## -------------------  TESTE CREDENCIAIS DO USUÁRIO    ------------------ ##
     if teste_log == USUARIO and teste_senha == SENHA:
         print('Login realizado com sucesso!')
+        menu()
     else:
         print('Usuário ou senha inválidos.')
-        ROTINA = int(input('Gostaria de sair? Digite 0 \n'))
+        ROTINA = int(input('Gostaria de sair? Digite 0 \n Tentar novamente? Digite 1 \n'))
         if ROTINA == 0:
             print('Programa encerrado.')
+        else:
+            ROTINA = 1
+
 
 
 
